df252794df1b842013dd60cf1a4def67255d59a3
Brennan Emerson <brennanemerson15@gmail.com>
2022-05-08 11:25:32 -0700

    Minor changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,22 +1,60 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Spotify from '../../utils/Spotify'
+import SearchBar from '../SearchBar/SearchBar';
+import SearchResults from '../SearchResults/SearchResults'
+import Playlist from '../Playlist/Playlist';
 
 function App() {
+  const [searchedTracks, setSearchedTracks] = useState([]);
+  const [playlistTracks, setPlaylistTracks] = useState([]);
+  const [spotifyToken, setSpotifyToken] = useState(null);
+
+  useEffect(() => {
+    const spotifyTokenFromUrlFragment = window.location.hash.split('&')[0].substr(14);
+    setSpotifyToken(spotifyTokenFromUrlFragment);
+  }, [])
+
+  async function searchSpotify(searchTerms) {
+      const results = await Spotify.search(searchTerms, spotifyToken);
+      setSearchedTracks(results);
+    }
+
+    async function createSpotifyPlaylist(name, trackIds) {
+      await Spotify.createPlaylist(name, trackIds, spotifyToken);
+      setPlaylistTracks([]);
+    }
+  
+  function addTrackToPlaylist(track) {
+    setPlaylistTracks(oldPlaylistTracks => {
+      if (oldPlaylistTracks.includes(track)) {
+        return oldPlaylistTracks;
+      }
+      else {
+        return [...oldPlaylistTracks, track];
+      }
+    });
+  }
+
+  function removeTrackFromPlaylist(track) {
+    setPlaylistTracks(oldPlaylistTracks => oldPlaylistTracks.filter((t => track !== t)));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img  className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <a href="https://github.com/Bemerson95/Jammming" alt="Jamming GitHub Repo">
+        <img src="./github.png" alt="GitHub Mark" height="56" align="right"/>
+      </a>
+      <h1>Ja<span className="highlight">mm</span>ing</h1>
+      <div className="App">
+        <SearchBar searchSpotify={searchSpotify}/>
+        <div className="App-playlist">
+          <SearchResults tracks={searchedTracks} addTrackToPlaylist={addTrackToPlaylist}/>
+          <Playlist tracks={playlistTracks}
+          removeTrackFromPlaylist={removeTrackFromPlaylist}
+          createSpotifyPlaylist={createSpotifyPlaylist}/>
+        </div>
+      </div>
     </div>
   );
 }
